f20f361841508d10ff585c1124dbd2458e3c6322
Mamadou Mbaye <81474777+mmbaye90@users.noreply.github.com>
2021-10-29 20:42:26 +0200

    Intégration données par défaut avec appel à l'api

--- app.js
+++ app.js
@@ -396,3 +396,26 @@
 // longMot(mot)
 
 ///////////////////////////////////////////  API METEO ////////////////////////////////////////////////
+const apiKey = "8c2c1dc972a910e174bdce770934f494";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric&lang=fr`;
+fetch(url)
+    .then(
+        res => res.json()
+        .then(datas => {
+            console.log("-------------- Retour données API ---------------")
+            console.log(datas)
+            let town = document.getElementById("town")
+            town.innerHTML = datas.name
+
+            let temp = document.getElementById("temp")
+            let icon = `<i class="fas fa-thermometer-three-quarters"></i>`
+            temp.innerHTML = icon + datas.main.temp + "°"
+            let vent = document.getElementById("vent")
+            let iconVent = `<i class="fas fa-wind"></i>`
+            vent.innerHTML = iconVent + datas.wind.speed + " KM/H"
+            let humidity = document.getElementById("humidity")
+            let iconHumidity = `<i class="fas fa-tint"></i>`
+            humidity.innerHTML = iconHumidity + datas.main.humidity + " %"
+        })
+        .catch(err => console.log(err))
+    )
