e453b8756a496af90bc498126026739fc443eb01
Mamadou Mbaye <81474777+mmbaye90@users.noreply.github.com>
2021-10-30 21:47:35 +0200

    Nouveau api météo( intégration html finie

--- app.js
+++ app.js
@@ -396,48 +396,48 @@
 // longMot(mot)
 
 ///////////////////////////////////////////  API METEO ////////////////////////////////////////////////
-const apiKey = "8c2c1dc972a910e174bdce770934f494";
-const appelApi = function(v) {
+// const apiKey = "8c2c1dc972a910e174bdce770934f494";
+// const appelApi = function(v) {
 
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${v}&appid=${apiKey}&units=metric&lang=fr`;
-    fetch(url)
-        .then(
-            res => res.json()
-            .then(datas => {
+//     let url = `https://api.openweathermap.org/data/2.5/weather?q=${v}&appid=${apiKey}&units=metric&lang=fr`;
+//     fetch(url)
+//         .then(
+//             res => res.json()
+//             .then(datas => {
 
-                let town = document.getElementById("town")
-                town.innerHTML = datas.name
+//                 let town = document.getElementById("town")
+//                 town.innerHTML = datas.name
 
-                let temp = document.getElementById("temp")
-                let icon = `<i class="fas fa-thermometer-three-quarters"></i>`
-                temp.innerHTML = icon + datas.main.temp + "°"
-                let vent = document.getElementById("vent")
-                let iconVent = `<i class="fas fa-wind"></i>`
-                vent.innerHTML = iconVent + datas.wind.speed + " KM/H"
-                let humidity = document.getElementById("humidity")
-                let iconHumidity = `<i class="fas fa-tint"></i>`
-                humidity.innerHTML = iconHumidity + datas.main.humidity + " %";
+//                 let temp = document.getElementById("temp")
+//                 let icon = `<i class="fas fa-thermometer-three-quarters"></i>`
+//                 temp.innerHTML = icon + datas.main.temp + "°"
+//                 let vent = document.getElementById("vent")
+//                 let iconVent = `<i class="fas fa-wind"></i>`
+//                 vent.innerHTML = iconVent + datas.wind.speed + " KM/H"
+//                 let humidity = document.getElementById("humidity")
+//                 let iconHumidity = `<i class="fas fa-tint"></i>`
+//                 humidity.innerHTML = iconHumidity + datas.main.humidity + " %";
 
 
 
-            })
-            .catch(err => {
-                window.location = "index.html"
-            })
+//             })
+//             .catch(err => {
+//                 window.location = "index.html"
+//             })
 
-        )
-}
+//         )
+// }
 
-appelApi("paris")
-let form = document.getElementById("form-group")
-form.addEventListener("submit", function(e) {
-    e.preventDefault()
-    let inputUser = form.ville.value
-    let regexInput = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/
-    if (regexInput.test(inputUser)) {
-        appelApi(inputUser)
-    }
+// appelApi("paris")
+// let form = document.getElementById("form-group")
+// form.addEventListener("submit", function(e) {
+//     e.preventDefault()
+//     let inputUser = form.ville.value
+//     let regexInput = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/
+//     if (regexInput.test(inputUser)) {
+//         appelApi(inputUser)
+//     }
 
-})
+// })
 
 //------------------------------------------ Appel avec saisie de l'utilisateur ---------------------------
